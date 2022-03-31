72063f2483a0c597f3186b279e3bce160323148e
Omri Ruvio <omriruvio@gmail.com>
2022-03-31 03:25:02 +0300

    add pause and stop functionality

--- src/components/StopWatch.jsx
+++ src/components/StopWatch.jsx
@@ -13,7 +13,8 @@ export default class StopWatch extends React.Component {
       },
       start: 0,
       pauseTime: 0,
-      running: false
+      running: false,
+      diff: 0
     }
   }
 
@@ -24,55 +25,45 @@ export default class StopWatch extends React.Component {
 
   handleStart = () => {
     const now = Date.now();
-    console.log('now', now, typeof now)
     this.setState({
-      ...this.state,
       running: true,
-      start: now,
+      start: this.state.pauseTime ? now - (this.state.pauseTime * 1000 ) : now,
       
     })
-    console.log(this.state.start);
   }
   handlePause = () => {
-    console.log('stopwatch pause')
     this.setState({
-      running: false
+      running: false,
+      pauseTime: (Date.now() - this.state.start) / 1000,
     })
   }
   handleStop = () => {
-    console.log('stopwatch stop')
     this.setState({
-      ...this.state,
       running: false,
       time: {
         hours: 0,
         minutes: 0,
         seconds: 0
       },
-      start: 0
+      start: 0,
+      pauseTime: 0,
     })
   }
 
   componentDidUpdate = () => {
-    if (this.state.running) {
-      requestAnimationFrame(() => {
+    setTimeout(() => {
+      if (this.state.running) {
         const diff = parseInt((Date.now() - this.state.start) / 1000);
         this.setState({
           time: {
             seconds: diff % 60,
             minutes: parseInt((diff % 3600) / 60),
             hours: parseInt(diff / 3600)
-          }
+          },
+          diff
         })
-      })
-    }
-  }
-
-  componentWillUnmount = () => {
-
-  }
-  componentDidMount = () => {
-    
+      }
+    }, 1000)
   }
 
   render () {
